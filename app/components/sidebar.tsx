5ec3f033931d8a00553672a6397884fba6c1883d
d0j1a_1701 <dajia1701@gmail.com>
2023-12-25 23:42:09 +0800

    更名

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          ChatGPT Mirror
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          d0j1a_1701 的私有 ChatGPT 镜像。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT Mirror",
+  description: "d0j1a_1701 的私有 ChatGPT 镜像。",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "ChatGPT Mirror",
     statusBarStyle: "default",
   },
 };
